915dba45381ed263b9dd1678b8bcb52d1d87e5b9
tsutsu <tsutsu0928@gmail.com>
2016-09-04 18:32:05 +0900

    枠なしのウィンドウを表示．

--- cappuccino.py
+++ cappuccino.py
@@ -1,10 +1,25 @@
+import sys
 from downloader import download_image
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtCore import Qt
+
+def download():
+    keyword = '美女'
+    dirname  = 'image'
+    minsize = (500, 500)
+    download_image(keyword, 100, dirname, minsize)
+
+class MainWindow(QWidget):
+    def __init__(self, parent = None):
+        super(MainWindow, self).__init__(parent, Qt.FramelessWindowHint)
+        self.resize(500, 500)
+        self.setWindowTitle("cappuccino")
 
 def main():
-    keyword = 'ご注文はうさぎですか？？'
-    dirname  = 'image'
-    minsize = (400, 400)
-    download_image(keyword, 100, dirname, minsize)
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
     main()
